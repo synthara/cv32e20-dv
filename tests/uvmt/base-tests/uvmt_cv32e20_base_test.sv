17eea537e7560a3e3c048fe1ade14f107109a374
PaoloBorgis-synthara <paolo.borgis@synthara.ai>
2025-06-13 15:06:51 +0200

    Fixed wfi in all tests and number minimum errors set back to 5

--- tests/uvmt/base-tests/uvmt_cv32e20_base_test.sv
+++ tests/uvmt/base-tests/uvmt_cv32e20_base_test.sv
@@ -212,7 +212,7 @@ function void uvmt_cv32e20_base_test_c::build_phase(uvm_phase phase);
 
    super.build_phase(phase);
 
-   rs.set_max_quit_count(.count(1000000), .overridable(1));
+   rs.set_max_quit_count(.count(5), .overridable(1));
 
    retrieve_vifs    ();
    create_cfg       ();
